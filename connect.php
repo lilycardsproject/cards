a3b9671737f62ab0ed8a5b2f18c7651b839c4c9e
lilycardsproject <lilycards@protonmail.com>
2019-10-08 13:43:50 +0300

    connect.php is done

---
+++ connect.php
@@ -0,0 +1,19 @@
+<?php
+
+function connect_db() {
+  $servername = "localhost";
+  // EDIT HERE YOUR USERNAME AND PASSWORD
+  $username = "a1800601";
+  $password = "byHYDn67p";
+  $dbname = "cards";
+
+  // Create connection
+  $conn = new mysqli($servername, $username, $password, $dbname)
+  // Check connection
+  if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+  } else {
+  }
+  return $conn;
+}
+?>
